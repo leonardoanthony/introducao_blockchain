ee0076c4da7f2a75f22b8b7c2ac8296074ad443c
Leonardo Anthony <leonardoanthony.dev@gmail.com>
2023-09-01 10:32:33 -0300

    :sparkles: feat: #12. Conectando na blockchain via MetaMask

--- src/aula_12/script.js
+++ src/aula_12/script.js
@@ -1,4 +1,8 @@
-
+async function getContractInitData(){
+    const endereco = '0xa379d2ADd94b5772FF3f57da10C88Aa6cD7b7398';
+    const {abi} = await (await fetch('../../build/contracts/Padrao.json')).json();
+    return {abi, endereco};
+}
 
 async function conectar() {
     const result = await ethereum.request({method: 'eth_requestAccounts'});
@@ -12,8 +16,7 @@ async function getAccount(web3) {
 
 async function pegaValor() {
     const web3 = new Web3(window.ethereum);
-    const endereco = '0x2Ee8AFF291131e49fc65edf6A0203429721f8AC0';
-    const {abi} = await (await fetch('../../build/contracts/Padrao.json')).json();
+    const {abi, endereco} = await getContractInitData();
 
     const contract = new web3.eth.Contract(abi, endereco);
 
@@ -24,34 +27,32 @@ async function pegaValor() {
 
 async function mudaValor() {
     const web3 = new Web3(window.ethereum);
-    const endereco = '0x2Ee8AFF291131e49fc65edf6A0203429721f8AC0';
-    const {abi} = await (await fetch('../../build/contracts/Padrao.json')).json();
-
+    const {abi, endereco} = await getContractInitData();
 
     const account = (await web3.eth.getAccounts())[0];
       
 
     const contract = new web3.eth.Contract(abi, endereco);
-    let receipt = await contract.methods.mudaNome('Leonardo').send({from: account, gas: '3000000'});
+    let receipt = await contract.methods.mudaNome('Leonardo').send({from: account, gas: '45040'});
     console.log(receipt);
-
-
-    // contract.methods.mudaNome('Leonardo').estimateGas({from: account,})
-    //     .then(function(gasAmount){
-    //         console.log(Number(gasAmount));
-    //     })
-    //     .catch(function(error){
-    //         console.log(error);
-    //     });
-
-
 }
 
+async function getGas() {
+    const web3 = new Web3(window.ethereum);
+    const {abi, endereco} = await getContractInitData();
+    const account = (await web3.eth.getAccounts())[0];
+    const contract = new web3.eth.Contract(abi, endereco);
 
-
-
+    contract.methods.mudaNome('Leonardo').estimateGas({from: account,})
+        .then(function(gasAmount){
+            console.log(Number(gasAmount));
+        })
+        .catch(function(error){
+            console.log(error);
+    });
+}
 
 document.querySelector('#conectar').addEventListener('click', conectar);
-// document.querySelector('#getAccount').addEventListener('click', getAccount);
+document.querySelector('#getGas').addEventListener('click', getGas);
 document.querySelector('#pegaValor').addEventListener('click', pegaValor);
 document.querySelector('#mudaValor').addEventListener('click', mudaValor);
